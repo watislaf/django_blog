78af71c30910f5d54a4aab4036316f9539397261
watislaf <vladkozulin@mail.ru>
2022-03-06 19:27:41 +0100

    small changes2

--- tmp.py
+++
@@ -1,4 +0,0 @@
-import plyvel
-
-db = plyvel.DB(r'C:\Users\ASUS\AppData\Roaming\Vampire_Survivors\Local Storage\leveldb', create_if_missing=False)
-db.put(b'_file://\x00\x01CapacitorStorage.Coins', b'\x0199999')
